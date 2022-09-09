bf3baa49fb0b9d95b01fd9879c9cc47a66e389ac
Albert Nutifafa Dovlo <albertnutifafa@gmail.com>
2022-09-08 21:34:54 -0700

    4. Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,19 @@
 #include <stdio.h>
+
 /**
- * main - entry point
  *
- * Return:0
- * /
- int main(void)
- {
-	 puts("\"Programming is like building a multilingual puzzle");
-	 return (0);
- }
+ *  * main - print the string in the put function
+ *
+ *   * Return: 0
+ *
+ *    */
+
+int main(void)
+
+{
+
+	puts("\"Programming is like building a multilingual puzzle");
+
+	return (0);
+
+}
