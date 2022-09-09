85afc505afeeb7ccc7104c669428d2cf67794b5d
Albert Nutifafa Dovlo <albertnutifafa@gmail.com>
2022-09-08 21:43:11 -0700

    5. Hello, printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+  * main - main block, this function should print the sentence
+  * Return: 0
+  */
+
+int main(void)
+
+{
+
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+
+	return (0);
+
+}
