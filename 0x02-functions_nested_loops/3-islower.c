da8d1e0c0fb385615fc2f24d49d19172591e1f51
Albert Nutifafa Dovlo <albertnutifafa@gmail.com>
2022-09-17 17:03:54 -0700

    3. islower

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,9 +1,13 @@
 #include <stdio.h>
 
 /**
- *   * main - sum even fibonacci numbers under 4 million.
+ *  * _islower - tests whether a character is a
  *
- *     * Return: Nothing.
+ *   * lowercase letter from the English alphabet.
+ *
+ *    * @c: character to test.
+ *
+ *     * Return: 1 if it is a lowercase letter, 0 if it is not a lowercase letter.
  *
  */
 
