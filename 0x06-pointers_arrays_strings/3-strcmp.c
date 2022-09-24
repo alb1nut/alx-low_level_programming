cd417c6cc9d0fa1137c435fa4b766e4b0fa15025
Albert Nutifafa Dovlo <albertnutifafa@gmail.com>
2022-09-24 06:17:16 -0700

    3. strcmp

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,8 +1,8 @@
 /**
- *  * _strcmp - compares two strings, ending at the first byte that is different.
+ * _strcmp - compares two strings, ending at the first byte that is different
  *   * @s1: string to be compared
  *    * @s2: string to compare by
- *     * Return: the difference between the first character that are not the same
+ * Return the difference between the first character that are not the same
  */
 
 int _strcmp(char *s1, char *s2)
@@ -10,5 +10,4 @@ int _strcmp(char *s1, char *s2)
 int i;
 for (i = 0; s1[i] != '\0' && s2[i] != '\0' && s1[i] == s2[i]; i++)
 ;
-return (s1[i] - s2[i]);
-}
+return (s1[i] - s2[i]}
