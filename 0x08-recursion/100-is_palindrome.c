7545ee24faee5fdb3470e778fd34633a8128abf3
Albert Nutifafa Dovlo <albertnutifafa@gmail.com>
2022-09-28 07:44:14 -0700

    7. They say we only use a fraction of our brain's true potential. Now that's when we're awake. When we're asleep, we can do almost anything

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,8 +1,50 @@
 #include "main.h"
 /**
- *  * _puts_recursion - Prints a string followed by a new line
+ *  * palind2 - obtains length of a
  *
- *   * @s: string
+ *   * @a: string
+ *
+ *    * @l: integer to count length
+ *
+ *     *
+ *
+ *      * Return: On success 1.
+ *
+ *       * On error, -1 is returned, and errno is set appropriately.
+ *
+ */
+int palind2(char *a, int l)
+{
+if (*a == 0)
+return (l - 1);
+return (palind2(a + 1, l + 1));
+}
+/**
+ *  * palind3 - compares string vs string reverse
+ *
+ *   * @a: string
+ *
+ *    * @l: length
+ *
+ *     *
+ *
+ *      * Return: On success 1.
+ *
+ *       * On error, -1 is returned, and errno is set appropriately.
+ *
+ */
+int palind3(char *a, int l)
+{
+if (*a != *(a + l))
+return (0);
+else if (*a == 0)
+return (1);
+return (palind3(a + 1, l - 2));
+}
+/**
+ *  * is_palindrome - checks if a string is a palindrome
+ *
+ *   * @s: string to evaluate
  *
  *    *
  *
@@ -11,13 +53,9 @@
  *      * On error, -1 is returned, and errno is set appropriately.
  *
  */
-void _puts_recursion(char *s)
+int is_palindrome(char *s)
 {
-if (*s == 0)
-{
-_putchar('\n');
-return;
-}
-_putchar(*s);
-_puts_recursion(s + 1);
+int l;
+l = palind2(s, 0);
+return (palind3(s, l));
 }
