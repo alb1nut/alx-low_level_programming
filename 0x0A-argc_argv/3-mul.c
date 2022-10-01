c7d5b1ef6cdf04eba41e3112c261ab0419015b0a
Albert Nutifafa Dovlo <albertnutifafa@gmail.com>
2022-09-30 19:15:56 -0700

    3. Neither irony nor sarcasm is argument

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,14 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 /**
- *  * main - multiplies two numbers and prints the result
+ * main - multiplies two numbers and prints the result
  *
  *   * @argc: argument count must be exactly two numbers
  *
  *    * @argv: argument vector
  *
  *     * Return: 0 on success, 1 on failure
- *
  */
 int main(int argc, char *argv[])
 {
